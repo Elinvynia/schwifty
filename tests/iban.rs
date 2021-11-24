1f801bba311701ec73db0769047690642471b5b9
Terah <terah.dev@gmail.com>
2021-11-24 13:25:07 +0100

    Fix a panic when the provided iban length is less than 2 characters

--- tests/iban.rs
+++ tests/iban.rs
@@ -83,3 +83,8 @@ fn validate_iban_basic() {
     validate("IQ98 NBIQ 8501 2345 6789 012").unwrap(); // Iraq
     validate("AA11 0011 123Z 5678").unwrap(); // Internet
 }
+
+#[test]
+fn validate_iban_empty() {
+    assert!(matches!(validate(""), Err(ValidationError::InvalidCountryCode)));
+}
